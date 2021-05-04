f17ba275d92c7cd16fdcd5444753782761b185f5
eluttrell <elieel@gmail.com>
2021-05-04 16:09:35 -0400

    Update mock data complexity

--- employee-hierarchy-count.js
+++ employee-hierarchy-count.js
@@ -30,26 +30,55 @@ const employees = [
     id: 5,
     managerId: 2,
   },
+  {
+    name: 'g',
+    id: 11,
+    managerId: 10,
+  },
+  {
+    name: 'h',
+    id: 12,
+    managerId: 11,
+  },
+  {
+    name: 'i',
+    id: 13,
+    managerId: 12,
+  },
+  {
+    name: 'j',
+    id: 14,
+    managerId: 13,
+  },
+  {
+    name: 'k',
+    id: 15,
+    managerId: 14,
+  },
+  {
+    name: 'l',
+    id: 16,
+    managerId: 14,
+  },
 ];
 
 function employeeReportsCount(arr) {
   let managers = {};
   arr.forEach((employee) => {
-    if (!managers[employee.name]) managers[employee.name] = 0;
     crawlHierarchy(employee, arr, managers)
   });
   return managers;
 }
 
 function crawlHierarchy(employee, arr, managers) {
+  if (!managers[employee.name]) managers[employee.name] = 0;
   if (employee.managerId) {
     const manager = arr.find(el => el.id === employee.managerId);
     if (!managers[manager.name]) managers[manager.name] = 0;
     managers[manager.name]++;
     return crawlHierarchy(manager, arr, managers);
-  } else {
-    return;
-  }
+  } 
+  return;
 }
 
 console.log(employeeReportsCount(employees));
